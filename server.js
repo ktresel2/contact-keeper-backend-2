dd86a7b4106807e82d9e17507d25ddd9e9f0388e
Kevin Treseler <kevintreseler@MacBook-Pro.lan>
2023-06-07 06:42:02 -0400

    Add authentication

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express')
-
+const connectDB = require('./config/db')
+const cors = require('cors')
 const app = express()
+
+// cors
+// app.use(
+// 	cors({
+// 		origin: '*',
+// 	})
+// )
+
+// Connect DB
+connectDB()
+
+// Init Middleware
+app.use(express.json({extended:false}))
 
 app.get('/', (req, res) => res.json({msg: 'Welcome'}))
 
